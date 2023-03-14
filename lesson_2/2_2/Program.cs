658ec3cda7f38a4eb50ef6f85502a9cfc634e8be
Konstantin <kashin-1968@mail.ru>
2023-03-15 02:08:10 +0300

    Task 2_2

---
+++ lesson_2/2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+
+void divide(int nm1, int nm2)
+{
+    if (nm1 % nm2 == 0)
+    {
+        Console.WriteLine($"Число {nm2} кратно {nm1}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {nm2} не кратно {nm1}");
+        Console.WriteLine(nm1 % nm2);
+    }
+} 
+divide(num1, num2);
