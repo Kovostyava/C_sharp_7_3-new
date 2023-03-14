7d025eedee11dd908f061874af6597c2bae3775a
Konstantin <kashin-1968@mail.ru>
2023-03-15 02:01:02 +0300

    Task 2_1

---
+++ lesson_2/1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int Num(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 10;
+    int num2 = num1 % 10;
+    if (num1 > num2) return num1;
+    return num2;
+}
+int a = Num(new Random().Next(10, 100));
+Console.WriteLine(a);
