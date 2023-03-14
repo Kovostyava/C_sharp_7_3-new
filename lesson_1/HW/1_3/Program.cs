3dd0add8a268e7deeb5d209f8e7fd40c8a829c8e
Konstantin <kashin-1968@mail.ru>
2023-03-15 01:29:25 +0300

    Task hw 1_3

---
+++ lesson_1/HW/1_3/Program.cs
@@ -0,0 +1,10 @@
+﻿int n_1 = int.Parse(Console.ReadLine()!);
+
+if (n_1 % 2 == 0)
+{
+    Console.Write($"The number {n_1} is even");
+}
+else
+{
+    Console.Write($"The number {n_1} is uneven");
+}
