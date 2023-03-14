c687e7e0a7d3fe507bbeb3d159de654f4d4d362c
Konstantin <kashin-1968@mail.ru>
2023-03-15 00:55:40 +0300

    Task 1_1

---
+++ lesson_1/1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Write a number:");
+
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Write a number:");
+
+int b = int.Parse(Console.ReadLine()!);
+
+if (a == b * b)
+
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
