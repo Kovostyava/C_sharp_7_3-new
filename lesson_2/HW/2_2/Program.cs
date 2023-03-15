6e90a8dbb487e9df1851a56230f47fbf4b1f2a82
Konstantin <kashin-1968@mail.ru>
2023-03-15 14:51:26 +0300

    Task hw 2_2

---
+++ lesson_2/HW/2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, оторая выводит случайное
+// трехзначное число и удаляет вторую цифру этого числа.
+
+string SecondNum(int num)
+{
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+        return $"[num] -> {num / 100 * 10 + num % 10}";
+    return "The number is not three-digit!";
+}
+
+int val = int.Parse(Console.ReadLine()!);
+string result = SecondNum(val);
+Console.WriteLine(result);
