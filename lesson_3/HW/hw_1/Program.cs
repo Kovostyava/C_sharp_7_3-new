a7332403daf9827fa1988205c87e929a3245d7c5
Konstantin <kashin-1968@mail.ru>
2023-03-17 21:17:30 +0300

    hw 3_1

---
+++ lesson_3/HW/hw_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. Напишите программу, которая принимает на вход
+// пятизначное число и проверяет, явлется ли оно полиндромом.
+
+void Pali(int num)
+{
+    int num_1_2 = num / 1000;
+    int num_5 = num % 10;
+    int num_4 = num / 10 % 10;
+
+    if (num_1_2 == num_5 * 10 + num_4)
+        Console.WriteLine($"yes, {num} is a palindrom");
+    else
+        Console.WriteLine($"no, {num} is not a polindrom");
+
+}
+
+Console.WriteLine("Enter a five-digit number");
+int val = int.Parse(Console.ReadLine()!);
+
+Pali(val);
