ab6df16c01f0eb80adf362e51240c42ab8d93743
Konstantin <kashin-1968@mail.ru>
2023-03-21 16:12:15 +0300

    hw_2

--- lesson_4/HW/hw_2/Program.cs
+++ lesson_4/HW/hw_2/Program.cs
@@ -1,2 +1,16 @@
 ﻿// 2. Напишите программу, которая принимает на вход число и выдаёт 
 // сумму цифр в числе.
+
+int SumNums_2(int num)
+{
+    int n_sum = 0;
+
+    for (; num != 0; num /= 10)
+        n_sum += num % 10;
+
+    return n_sum;
+}
+
+
+int num_2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNums_2(num_2));
