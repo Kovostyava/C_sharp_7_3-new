cbf76f9ceb94a8710e34afee5e34e8a37a724532
Konstantin <kashin-1968@mail.ru>
2023-03-24 13:47:49 +0300

    Task 6_2

--- lesson_6/6_2/Program.cs
+++ lesson_6/6_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+string binary(int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    return result;
+}
+
+Console.Write("enter a number:");
+int x = int.Parse(Console.ReadLine()!);
+string answer = binary(x);
+Console.WriteLine(answer);
