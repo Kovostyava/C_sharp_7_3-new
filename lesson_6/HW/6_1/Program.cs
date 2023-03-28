bf643cce6b3391ffb65599378a5fcbfaceed53d5
Konstantin <kashin-1968@mail.ru>
2023-03-28 12:14:36 +0300

    hw 6_1

---
+++ lesson_6/HW/6_1/Program.cs
@@ -0,0 +1,55 @@
+﻿// 1. Напишите программу, которая будет создавать копию заданного
+//    массива с помощью поэлементного копирования.
+
+
+
+void PrintArray(int[,] matr)
+
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+    
+         {
+            matr[i, j] = new Random().Next(1, 10);
+         }
+
+    }
+}
+
+void MyArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+    
+         {
+            matr[i, j] = new Random().Next(1, 10);
+         }
+
+    }
+}
+
+int[,] matrix = new int[4, 5];
+PrintArray(matrix);
+FillArray(matrix);
+MyArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+MyArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
