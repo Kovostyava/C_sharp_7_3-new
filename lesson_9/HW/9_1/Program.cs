c61b5415c41ce397ad75d3a38af38c4b5d6890c1
Konstantin <kashin-1968@mail.ru>
2023-04-08 19:40:38 +0300

    hw 9_1

--- lesson_9/HW/9_1/Program.cs
+++ lesson_9/HW/9_1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// 1. Задайте значения M и N. Напишите программу, которая выведет все чётные
 //    натуральные числа в промежутке от M до N с помощью рекурсии.
 
-Console.Write("Введите значение M: ");
+Console.Write($"Enter number M: ");
 int m = Convert.ToInt32(Console.ReadLine()!);
-Console.Write("Введите значение N: ");
+Console.Write($"Enter number N: ");
 int n = Convert.ToInt32(Console.ReadLine()!);
 void che(int m, int n)
 {
