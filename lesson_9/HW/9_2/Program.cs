53f4f2618657c681e70445b86ca37c877526fcb1
Konstantin <kashin-1968@mail.ru>
2023-04-08 19:50:56 +0300

    hw 9_2

--- lesson_9/HW/9_2/Program.cs
+++ lesson_9/HW/9_2/Program.cs
@@ -2,4 +2,30 @@
 //     натуральных элементов в промежутке от M до N с помощью рекурсии.
 
 
+Console.Write($"Enter number M: ");
+int m = Convert.ToInt32(Console.ReadLine()!);
 
+Console.Write($"Enter number N: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+ 
